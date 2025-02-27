ed119a166a3db70630d864c79727d169a4d04b40
Krzysztof Mickiewicz <107584697+Warmick91@users.noreply.github.com>
2025-02-27 18:39:13 +0100

    Jwt auth angular (#2)
    * wip: moved from localStorage to cookies fot JWT
    
    * wip: Log-in in Angular, CORS update

--- TheClickPot.Portal/src/components/pot-auth/pot-auth.component.ts
+++ TheClickPot.Portal/src/components/pot-auth/pot-auth.component.ts
@@ -1,9 +1,11 @@
 import { JsonPipe } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Button } from 'primeng/button';
 import { Card } from 'primeng/card';
 import { IftaLabelModule } from 'primeng/iftalabel';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-pot-auth',
@@ -14,15 +16,40 @@ import { IftaLabelModule } from 'primeng/iftalabel';
 export class PotAuthComponent {
   email = '';
   password = '';
+  errorMessage = '';
 
-  login(loginForm: NgForm) {
-    console.log('(TEST) Logged in successfully with data:', loginForm.value);
+  constructor(private readonly _authService: AuthService) {}
+
+  login() {
+    this._authService.login({ email: this.email, password: this.password }).subscribe({
+      next: () => {
+        console.log('(SUCCESS) User logged in successfully!');
+        this.errorMessage = '';
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log('(ERROR) User login failed: ', err);
+        this.errorMessage = this.getErrorMessage(err);
+      },
+    });
+  }
+
+  logout() {
+    this._authService.logout();
   }
 
   cancel(loginForm: NgForm) {
     console.log('(TEST) Submit cancelled. Input fields cleared.');
-    // this.email = '';
-    // this.password = '';
     loginForm.reset();
+    this.errorMessage = '';
+  }
+
+  getErrorMessage(error: HttpErrorResponse): string {
+    if (error.status === 401) {
+      return 'Invalid email or password. Please try again.';
+    } else if (error.status === 500) {
+      return 'Server error. Please try again later.';
+    } else {
+      return 'An unknown error occurred. Please try again.';
+    }
   }
 }
