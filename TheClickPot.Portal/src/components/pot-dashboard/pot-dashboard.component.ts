215ebde06848361801ea9ce9c61e0a2601e61449
Krzysztof Mickiewicz <107584697+Warmick91@users.noreply.github.com>
2025-02-28 18:37:14 +0100

    feat: AuthGuard works (#4)

---
+++ TheClickPot.Portal/src/components/pot-dashboard/pot-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pot-dashboard',
+  imports: [],
+  templateUrl: './pot-dashboard.component.html',
+  styleUrl: './pot-dashboard.component.scss'
+})
+export class PotDashboardComponent {
+
+}
