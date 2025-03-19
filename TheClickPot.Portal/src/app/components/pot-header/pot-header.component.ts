8458693d921ec034dbb2bf3e0753b30baf0a474e
Krzysztof <mickiewicz.dbass@gmail.com>
2025-03-19 22:14:26 +0100

    wip: reworked login along the way

--- TheClickPot.Portal/src/app/components/pot-header/pot-header.component.ts
+++ TheClickPot.Portal/src/app/components/pot-header/pot-header.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuItem, PrimeIcons } from 'primeng/api';
 import { AvatarModule } from 'primeng/avatar';
 import { MenubarModule } from 'primeng/menubar';
 import { AppStore } from '../../core/app.store';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-pot-header',
@@ -13,4 +15,12 @@ import { AppStore } from '../../core/app.store';
 })
 export class PotHeaderComponent {
   appStore = inject(AppStore);
+  authService = inject(AuthService);
+
+  readonly loginMenuItem: MenuItem = { label: 'Login', icon: PrimeIcons.USER, routerLink: ['/auth'] };
+  readonly logoutMenuItem: MenuItem = { label: 'Logout', icon: PrimeIcons.SIGN_OUT, routerLink: ['/auth'] };
+
+  isAuthenticated(): boolean {
+    return this.authService.authSignal();
+  }
 }
