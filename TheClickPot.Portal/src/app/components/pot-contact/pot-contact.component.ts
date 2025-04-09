ba75480d24dde80aa2a834d66298d3b72aa0f718
Krzysztof <mickiewicz.dbass@gmail.com>
2025-04-09 13:19:54 +0200

    wip: left box base structure ready, right box wip

--- TheClickPot.Portal/src/app/components/pot-contact/pot-contact.component.ts
+++ TheClickPot.Portal/src/app/components/pot-contact/pot-contact.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Button } from 'primeng/button';
+import { IftaLabelModule } from 'primeng/iftalabel';
 
 @Component({
   selector: 'app-pot-contact',
-  imports: [],
+  imports: [FormsModule, Button, IftaLabelModule],
   templateUrl: './pot-contact.component.html',
   styleUrl: './pot-contact.component.scss',
 })
-export class PotContactComponent {}
+export class PotContactComponent {
+  email = '';
+  message = '';
+  errorMessage = '';
+
+  public sendMessage() {
+    console.log('Sending message to mail...');
+  }
+
+  public cancel(messageForm: NgForm) {
+    console.log('Cancelling message...');
+    messageForm.reset();
+    this.errorMessage = '';
+  }
+}
